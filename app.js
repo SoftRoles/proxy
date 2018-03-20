0b4713a2de50a4280dec2fba6f767f176838cd4b
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2018-03-21 00:05:21 +0200

    Routes redefined. Test: size column included.

--- app.js
+++ app.js
@@ -16,24 +16,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.static(__dirname + "/test"))
 app.listen(3001, function () {
-    notifier.notify({
-        title: 'Service: 3001-dirtree',
-        message: "Started succesfully!",
-        wait: true
-    });
+  notifier.notify({
+    title: 'Service: 3001-dirtree',
+    message: "Started succesfully!",
+    wait: true
+  });
 })
 
-app.get("/", function (req, res) {
-    res.sendFile("test/index.html", { root : __dirname})
+app.get("/dirtree", function (req, res) {
+  res.send(dirTree(req.query.path))
 })
 
-app.post("/", function(req, res){
-	console.log(req.body.path)
-    res.send(dirTree(req.body.path))
-})
-
-app.get("/forwebixtreetable", function (req, res) {
-	res.send(dirTree(req.query.path))
-})
-
-console.log(dirTree('c:\\Users\\Ev\\Desktop'))
