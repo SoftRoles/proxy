2e44fe33ba6e3788ed3638210dad29dff2f9344e
hsyn <yigit.hsyn>
2018-05-08 10:52:48 +0000

    Some tests

--- transparent-proxy-route.js
+++ transparent-proxy-route.js
@@ -1,6 +1,6 @@
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxy(); 
-var options = { pathnameOnly: true, router: { '/wiki': '127.0.0.1:3004'} }
+var options = { pathnameOnly: true, router: { '/webssh': 'http://127.0.0.1:443'} }
 
 var proxyServer = httpProxy.createServer(options); proxyServer.listen(80);
 
