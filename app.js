b68eb42576ea95c8255305cad4692c4a1f48d2fe
hsyn <yigit.hsyn>
2018-05-09 11:15:31 +0000

    3005-mongodb proxy added.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var proxy = require('http-proxy-middleware');
 const app = express();
 app.use('/em', proxy({ target: 'http://localhost:5001', changeOrigin: true }));
 app.use('/dirtree', proxy({ target: 'http://localhost:3001', pathRewrite: {'^/dirtree' : ''}, changeOrigin: true }));
+app.use('/mongodb', proxy({ target: 'http://localhost:3005', pathRewrite: {'^/mongodb' : ''}, changeOrigin: true }));
 app.use('/webssh', proxy({ target: 'http://localhost:443', pathRewrite: {'^/webssh' : '/ssh/host/127.0.0.1'}, changeOrigin: true }));
 app.use('/socket.io', proxy({ target: 'http://localhost:443', changeOrigin: true }));
 app.use('/socket.io', proxy({ target: 'ws://localhost:443', changeOrigin: true, ws:true }));
