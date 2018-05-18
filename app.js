8f7548f472439669eed4c7b674fc523afa67e469
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2018-05-18 02:06:10 +0200

    functional app started up from older version

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-var fs = require("fs")
-var spawn = require("child_process").spawn
-var exec = require("child_process").exec
-
 var express = require('express')
 var bodyParser = require("body-parser")
 var cors = require("cors")
@@ -15,10 +11,11 @@ app.use('/mongodb', proxy({ target: 'http://localhost:3005', changeOrigin: true 
 app.use('/webssh', proxy({ target: 'http://localhost:443', pathRewrite: {'^/webssh' : '/ssh/host/127.0.0.1'}, changeOrigin: true }));
 app.use('/socket.io', proxy({ target: 'http://localhost:443', changeOrigin: true }));
 app.use('/socket.io', proxy({ target: 'ws://localhost:443', changeOrigin: true, ws:true }));
+app.use('/favorites', proxy({ target: 'http://localhost:8000', changeOrigin: true }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
-app.use(express.static(__dirname + "/test"))
+// app.use(express.static(__dirname + "/test"))
 app.listen(80, function () {
   console.log("Service running on http://127.0.0.1:80")
 })
