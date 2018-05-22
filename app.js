a67ccf36e1242b7483faa6fb52468accf769ae15
Hüseyin YİĞİT <yigit.hsyn@outlook.com>
2018-05-22 16:31:57 +0300

    https server activated.

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express')
 var bodyParser = require("body-parser")
 var cors = require("cors")
 
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
 var proxy = require('http-proxy-middleware');
 
 const app = express();
@@ -16,6 +24,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.static(__dirname + "/public"))
-app.listen(80, function () {
-  console.log("Service running on http://127.0.0.1:80")
-})
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
+
+// app.listen(80, function () {
+//   console.log("Service running on http://127.0.0.1:80")
+// })
