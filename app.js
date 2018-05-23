e63b5c80d9a21a39227f152df62e4d2b5c04bea9
Hüseyin YİĞİT <yigit.hsyn@outlook.com>
2018-05-23 16:59:41 +0300

    ide proxy added.

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use('/webssh', proxy({ target: 'http://localhost:3004', pathRewrite: {'^/web
 app.use('/socket.io', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
 app.use('/socket.io', proxy({ target: 'ws://localhost:3004', changeOrigin: true, ws:true }));
 app.use('/favorites', proxy({ target: 'http://localhost:8000', changeOrigin: true }));
+app.use('/ide', proxy({ target: 'http://localhost:8001', changeOrigin: true }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
