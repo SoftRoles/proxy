349de9c05d8f5b1ff0ffe1c7749dc0471e2db5e3
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2018-08-09 08:29:20 +0300

    webssh proxy removed

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use('/mongodb', proxy({ target: 'http://127.0.0.1:3005', changeOrigin: true 
 app.use('/login', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
 app.use('/logout', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
 app.use('/user', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
-app.use('/webssh', proxy({ target: 'http://127.0.0.1:3004', pathRewrite: {'^/webssh' : '/ssh/host/127.0.0.1'}, changeOrigin: true }));
 app.use("/favorites", express.static(path.join(os.homedir(), "desktop/Uygulamalar/8000-favorites")))
 app.use("/", express.static(path.join(os.homedir(), "desktop/Sayfalar/9000-antenio")))
 app.use(bodyParser.json())
