8a63a662444704457dece1a69db117eb9c624987
Hüseyin <yigit.hsyn@gmail.com>
2018-09-16 19:57:03 +0000

    npm install & file cleaning

--- transparent-proxy.js
+++
@@ -1,4 +0,0 @@
-var http = require('http'), httpProxy = require('http-proxy'); // // Create your proxy server and set the target in the options. // httpProxy.createProxyServ
-
-httpProxy.createProxyServer({target:'http://localhost:3004'}).listen(80); // See ()
-
