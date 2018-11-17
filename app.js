d47e034c84222ad420f03a3387215ef64f1cabbd
Hüseyin <yigit.hsyn@gmail.com>
2018-11-17 22:50:51 +0300

    bearer token pass in 3008 proxy

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var path = require('path');
 var proxy = require('http-proxy-middleware');
 
 const app = express();
+const proxyDefault = {changeOrigin: true, onProxyReq: (proxyReq, req) => {
+  if (req.user) proxyReq.setHeader("Authorization", "Bearer " + req.user.token);
+}}
 app.use('/filesystem', proxy({ target: 'http://127.0.0.1:3001', changeOrigin: true }));
 app.use('/webscrap', proxy({ target: 'http://127.0.0.1:3003', changeOrigin: true }));
 app.use('/mongodb', proxy({ target: 'http://127.0.0.1:3005', changeOrigin: true }));
@@ -16,17 +19,17 @@ app.use('/login', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true })
 app.use('/logout', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
 app.use('/user', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
 app.use('/403', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
-app.use('/serialport', proxy({ target: 'http://127.0.0.1:3008', changeOrigin: true }));
+app.use('/serialport', proxy(Object.assign({target: 'http://127.0.0.1:3008'}, proxyDefault)))
 app.use('/localcdn', proxy({ target: 'http://127.0.0.1:3009', changeOrigin: true }));
-if(process.platform == "win32"){
-	app.use("/", express.static(path.join(os.homedir(), "desktop/Sayfalar/9000-antenio")))
-	app.use("/favorites", express.static(path.join(os.homedir(), "desktop/Uygulamalar/8000-favorites")))
-	app.use("/serial", express.static(path.join(os.homedir(), "desktop/Uygulamalar/8002-serial")))
+if (process.platform == "win32") {
+  app.use("/", express.static(path.join(os.homedir(), "desktop/Sayfalar/9000-antenio")))
+  app.use("/favorites", express.static(path.join(os.homedir(), "desktop/Uygulamalar/8000-favorites")))
+  app.use("/serial", express.static(path.join(os.homedir(), "desktop/Uygulamalar/8002-serial")))
 }
-else{
+else {
   app.use("/", express.static(path.join(os.homedir(), "/Sayfalar/9000-antenio")))
-	app.use("/favorites", express.static(path.join(os.homedir(), "/Uygulamalar/8000-favorites")))
-	app.use("/serial", express.static(path.join(os.homedir(), "/Uygulamalar/8002-serial")))
+  app.use("/favorites", express.static(path.join(os.homedir(), "/Uygulamalar/8000-favorites")))
+  app.use("/serial", express.static(path.join(os.homedir(), "/Uygulamalar/8002-serial")))
 }
 
 app.use(bodyParser.json())
