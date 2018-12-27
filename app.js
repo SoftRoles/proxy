245afab4b680cf352d6f3dfd3c025a84e8aa4afe
Hüseyin <yigit.hsyn@gmail.com>
2018-12-27 22:53:52 +0300

    clean code

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express();
 
 // proxies
 var proxy = require('http-proxy-middleware');
-const proxyDefault = {changeOrigin: true, onProxyReq: (proxyReq, req) => {
-  if (req.user) proxyReq.setHeader("Authorization", "Bearer " + req.user.token);
-}}
 app.use('/filesystem', proxy({ target: 'http://127.0.0.1:3001', changeOrigin: true }));
 app.use('/webscrap', proxy({ target: 'http://127.0.0.1:3003', changeOrigin: true }));
 app.use('/mongodb', proxy({ target: 'http://127.0.0.1:3005', changeOrigin: true }));
@@ -13,7 +10,7 @@ app.use('/login', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true })
 app.use('/logout', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
 app.use('/user', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
 app.use('/403', proxy({ target: 'http://127.0.0.1:3007', changeOrigin: true }));
-app.use('/serialport', proxy(Object.assign({target: 'http://127.0.0.1:3008'}, proxyDefault)))
+app.use('/serialport', proxy({ target: 'http://127.0.0.1:3008', changeOrigin: true }));
 app.use('/localcdn', proxy({ target: 'http://127.0.0.1:3009', changeOrigin: true }));
 
 app.use('/socket/serialport', proxy({target: 'http://127.0.0.1:3008', pathRewrite:{"^/socket/serialport":"/socket.io"}, ws:true}))
