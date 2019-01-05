72b530feed3a85e828c58fee230a54a67c23e353
Hüseyin <yigit.hsyn@gmail.com>
2019-01-05 23:09:40 +0300

    pm2api testing finished.

--- draft/pm2api/app.js
+++ draft/pm2api/app.js
@@ -1,61 +1,89 @@
 const fs = require("fs")
 const path = require("path")
 
-// let dir = path.normalize(__dirname+"/..")
-// fs.readdir(dir, function(err, items){
-//   items.forEach(item=>{
-//     console.log(item,": ", fs.statSync(path.join(dir,item)).isDirectory())
-//   })
-// })
-
 const pm2 = require("pm2")
-
 pm2.connect(function (err) {
   if (err) {
     console.error(err);
     process.exit(2);
   }
-
-  // pm2.start(path.normalize(__dirname + "/../app.1.js"), { name: "test" }, function (err, proc) {
-  //   if (err) throw err
-  //   else {
-  //     console.log(proc)
-  //     pm2.list(function (err, items) {
-  //       items.forEach(item => { console.log(item.name) })
-  //     })
-  //     setTimeout(function(){pm2.disconnect()},5000)     
-  //   }
-
-  // })
-
-  // pm2.list(function(err,items){
-  //   items.forEach(item=>{console.log(item.name)})
-  // })
-  // setTimeout(pm2.disconnect,5000)
-});
+})
 
 var asyncLoop = require('node-async-loop');
- 
-var directories = ['test', 'test/hello', 'test/hello/world'];
-asyncLoop(directories, function (directory, next)
-{
-    fs.mkdir(directory, function (err)
-    {
-        if (err)
-        {
-            next(err);
-            return;
+
+const freePort = require("find-free-port")
+
+const argparse = require("argparse")
+  
+let dir = path.normalize(__dirname + "/../../..")
+fs.readdir(dir, function (err, items) {
+  let services = items.filter(item => {
+    let isDirectory = fs.statSync(path.join(dir, item)).isDirectory()
+    let isSelf = path.basename(path.normalize(__dirname + "/../..")) == item
+    return isDirectory && !isSelf
+  })
+  asyncLoop(services, function (service, next) {
+    freePort(3000, (err, port) => {
+      console.log(port)
+      pm2.start(path.join(dir, service, "app.js"), { name: service }, function (err, proc) {
+        if (err) throw err
+        else {
+          // console.log(proc)
+          pm2.list(function (err, items) {
+            items.forEach(item => { console.log(item.name) })
+          })
+          setTimeout(function () { next() }, 5000)
         }
- 
-        next();
-    });
-}, function (err)
-{
-    if (err)
-    {
-        console.error('Error: ' + err.message);
-        return;
-    }
- 
-    console.log('Finished!');
+      })
+    })
+  }, function (err) {
+    if (err) console.log(err)
+    else console.log("Finished.")
+    pm2.disconnect()
+  })
+  // console.log(services)
+  // items.forEach(item => {
+  // console.log(item, ": ", fs.statSync(path.join(dir, item)).isDirectory())
+  // })
+})
+
+// pm2.connect(function (err) {
+//   if (err) {
+//     console.error(err);
+//     process.exit(2);
+//   }
+
+// pm2.start(path.normalize(__dirname + "/../app.1.js"), { name: "test" }, function (err, proc) {
+//   if (err) throw err
+//   else {
+//     console.log(proc)
+//     pm2.list(function (err, items) {
+//       items.forEach(item => { console.log(item.name) })
+//     })
+//     setTimeout(function(){pm2.disconnect()},5000)     
+//   }
+
+// })
+
+// pm2.list(function(err,items){
+//   items.forEach(item=>{console.log(item.name)})
+// })
+// setTimeout(pm2.disconnect,5000)
+// });
+
+var ArgumentParser = argparse.ArgumentParser;
+var parser = new ArgumentParser({
+  version: '0.0.1',
+  addHelp: true,
+  description: 'Argparse example'
 });
+parser.addArgument(
+  ['-p', '--port'],
+  {
+    type: "int",
+    help: 'server listening port'
+  }
+);
+var args = parser.parseArgs();
+console.dir(args);
+console.dir(args.port);
