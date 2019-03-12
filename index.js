7865f7877f31a7481f2ed69e90c5a49184b69e06
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2019-03-12 11:51:07 +0000

    take proxy conf rom database

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 const fs = require('fs');
 const path = require('path');
+const assert = require('assert')
+const mongoClient = require("mongodb").MongoClient
+const mongodbUrl = "mongodb://127.0.0.1:27017"
+
+//-------------------------------------
+// proxy middlewares
+//-------------------------------------
+mongoClient.connect(mongodbUrl, { poolSize: 10, useNewUrlParser: true }, function (err, client) {
+  assert.equal(null, err);
+  client.db('softroles').collection('services').find({}).toArray((err,docs)=>{
+    assert.equal(null,err)
+    docs.forEach(service => {
+      console.log(`Service proxying: from ${service.name} to http://127.0.0.1:${service.port}`)
+      app.use(`/${service.name}`, proxy({ target: `http://127.0.0.1:${service.port}`, changeOrigin: true }));
+    })
+  })
+  client.db('softroles').collection('pages').find({}).toArray((err,docs)=>{
+    assert.equal(null,err)
+    docs.forEach(page => {
+      console.log(`Static page serving: from ${page.name} at ${page.path}`)
+      app.use(`/${page.path}`, express.static(path.normalize(`../../Pages/${page.name}`)))
+    })
+  })
+  client.db('softroles').collection('websockets').find({}).toArray((err,docs)=>{
+    assert.equal(null,err)
+    docs.forEach(ws => {
+      console.log(`Websocket proxying: from ${ws.name} to ws://127.0.0.1:${ws.port}`)
+      app.use(`/${ws.name}`, ws.proxy)
+    })
+  })
+})
 
 //=============================================================================
 // http server
@@ -17,35 +48,19 @@ const app = express();
 app.use(require("morgan")('tiny'))
 app.use(require("cors")())
 
-//-------------------------------------
-// proxy middlewares
-//-------------------------------------
-const services = JSON.parse(fs.readFileSync(path.join(__dirname,'services.json')))
-services.forEach(service => {
-  console.log(`Service proxying: from ${service.name} to http://127.0.0.1:${service.port}`)
-  app.use(`/${service.name}`, proxy({ target: `http://127.0.0.1:${service.port}`, changeOrigin: true }));
-})
-
-const pages = JSON.parse(fs.readFileSync(path.join(__dirname,'pages.json')))
-pages.forEach(page => {
-  console.log(`Static page serving: from ${page.name} at ${page.path}`)
-  app.use(`/${page.path}`, express.static(path.normalize(`../../Pages/${page.name}`)))
-})
-
-
-let websockets = JSON.parse(fs.readFileSync(path.join(__dirname,'websockets.json')))
-websockets.forEach(ws => {
-  ws.proxy = proxy(`ws://127.0.0.1:${ws.port}`)
-  console.log(`Websocket proxying: from ${ws.name} to ws://127.0.0.1:${ws.port}`)
-  app.use(`/${ws.name}`, ws.proxy)
-})
-
 //=============================================================================
 // start service
 //=============================================================================
 let server = app.listen(80, function () {
   console.log("Service running on http://127.0.0.1:80")
 })
-websockets.forEach(ws => {
-  server.on('upgrade', ws.proxy.upgrade)
+
+mongoClient.connect(mongodbUrl, { poolSize: 10, useNewUrlParser: true }, function (err, client) {
+  assert.equal(null, err);
+  client.db('softroles').collection('websockets').find({}).toArray((err,docs)=>{
+    assert.equal(null,err)
+    docs.forEach(ws => {
+      ws.proxy = server.on('upgrade', proxy(`ws://127.0.0.1:${ws.port}`).upgrade)
+    })
+  })
 })
